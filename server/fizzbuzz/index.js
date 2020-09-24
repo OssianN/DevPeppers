f55be422ecf0a6e97dcb3721612d0c424fc6a09e
jose rodrigues <jose@salt-admins-mbp.mynet>
2020-09-24 21:50:18 +0200

    Refactor of script.js, addition on eslint, small changes to other scripts

--- server/fizzbuzz/index.js
+++ server/fizzbuzz/index.js
@@ -1,27 +1,32 @@
-const fizzBuzz = arr => {
-    const newArr = new Array(arr);
-
-    return newArr.map(val => evalFizzbuzz(parseInt(val)));
-}
-
 const evalFizzbuzz = val => {
-    if (val.toString() === 'NaN') {
-        return 'Please provide a number, try again.';
-    } else if (val > 99) {
-        return 'Please provide a number below 100, try again.';
-    } else if (val < 1) {
-        return 'Please provide a number above 0, try again.';
-    } else if (val % 1 !== 0) {
-        return 'Please provide a number without decimals, try again.';
-    } else if (val % 3 === 0 && val % 5 === 0) {
-        return 'FizzBuzz';
-    } else if (val % 3 === 0) {
-        return 'Fizz';
-    } else if (val % 5 === 0) {
-        return 'Buzz';
-    } else {
-        return val;
-    }
-}
+  if (val.toString() === 'NaN') {
+    return 'Please provide a number, try again.';
+  }
+  if (val > 99) {
+    return 'Please provide a number below 100, try again.';
+  }
+  if (val < 1) {
+    return 'Please provide a number above 0, try again.';
+  }
+  if (val % 1 !== 0) {
+    return 'Please provide a number without decimals, try again.';
+  }
+  if (val % 3 === 0 && val % 5 === 0) {
+    return 'FizzBuzz';
+  }
+  if (val % 3 === 0) {
+    return 'Fizz';
+  }
+  if (val % 5 === 0) {
+    return 'Buzz';
+  }
 
-module.exports.evalFizzbuzz = evalFizzbuzz;
+  return val;
+};
+
+const fizzBuzz = arr => {
+  const newArr = new Array(arr);
+  return newArr.map(val => evalFizzbuzz(Number(val)));
+};
+
+module.exports.evalFizzbuzz = fizzBuzz;
--- server/static/static.js
+++ server/static/static.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
-const url = require('url');
 
 const base = `${__dirname}/../public`;
 
 function staticFiles(req, res, fileName, extension) {
-    console.log('=== FILE SYSTEM ===');
-    console.log(`${base}/${fileName}`);
   fs.readFile(`${base}/${fileName}`, 'utf8', (err, data) => {
     if (err) {
-     console.error('It failed');
+      console.error('It failed');
     } else {
       res.writeHead(200, { 'Content-Type': extension });
       res.end(data);
