d8bec25178176fc1dab3558dfc452fca338cfc77
jose rodrigues <jose@Salt-Admins-MacBook-Pro.local>
2020-09-25 09:54:09 +0200

    Final changes

--- server/routes.js
+++ server/routes.js
@@ -1,20 +1,13 @@
-const { primeFactors } = require('./prime');
-const { romanConverter } = require('./roman');
-const { evalFizzbuzz } = require('./fizzbuzz');
-const view = require('./static/static');
+const { primeFactors } = require('./api/prime');
+const { romanConverter } = require('./api/roman');
+const { evalFizzbuzz } = require('./api/fizzbuzz');
 
 const routes = (req, res) => {
   const queryString = req.url.split('?')[1];
   const key = queryString ? queryString.split('=')[0] : '';
   const value = queryString ? queryString.split('=')[1] : '';
 
-  if (req.url === '/') {
-    view.staticFiles(req, res, 'ourPage.html', 'text/html');
-  } else if (req.url === '/style.css') {
-    view.staticFiles(req, res, 'style.css', 'text/css');
-  } else if (req.url === '/script.js') {
-    view.staticFiles(req, res, 'script.js', 'text/html');
-  } else if (key === 'prime') {
+  if (key === 'prime') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     const data = primeFactors(value);
     res.end(JSON.stringify({ body: data }));
@@ -26,8 +19,6 @@ const routes = (req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     const data = romanConverter(value);
     res.end(JSON.stringify({ body: data }));
-  } else {
-    view.staticFiles(req, res, 'error/404.html', 'text/html', 404);
   }
 };
 
